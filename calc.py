23e9f272cefc46b617ec57a370c037d6403efffc
OLALYTICS <Larrysman2004@yahoo.com>
2025-07-14 13:13:37 +0100

    add subtraction feature

--- calc.py
+++ calc.py
@@ -4,6 +4,11 @@ def add(a, b):
     return a + b
 
 
+def sub(a, b):
+    return a - b
+
+
+
 
 
 
@@ -18,3 +23,5 @@ def add(a, b):
 
 if __name__ == "__main__":
     print(add(10, 20))
+    print(sub(10, 20))
+    
